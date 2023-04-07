24ba5a16371337ff3d621b56d7e5e57a956a7ea8
Luis Villegas <luism.villegasv@gmail.com>
2023-04-07 11:43:56 -0600

    Se hicieron responsive el canvas y sus elementos internos. Se importó maps.js

--- game.js
+++ game.js
@@ -1,7 +1,24 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+let canvasSize;
+let elementsSize;
+
+function setCanvasSize(){
+  canvasSize = (Math.min(window.innerHeight,window.innerWidth)*0.75)
+  // if (window.innerHeight>window.innerWidth){
+  //   canvasSize = window.innerWidth*0.75;
+  // }else{
+  //   canvasSize = window.innerHeight*0.75;
+  // }
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  elementsSize = (canvasSize/10)-1;
+  startGame();
+}
 
 function startGame(){
   // game.fillRect(x,y,w,h);
@@ -10,4 +27,9 @@ function startGame(){
   // game.fillStyle = 'purple';
   // game.textAlign = 'center';
   // game.fillText('Texto', x,y);
+  game.font = elementsSize+'px sans-seriff';
+  game.textAlign = 'start';
+  for (let i=0;i<10;i++){
+    game.fillText(emojis['X'], elementsSize*i, elementsSize);
+  }
 }
