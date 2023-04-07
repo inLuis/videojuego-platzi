38f8589ba5f0dc6975bf55a4836006313adeae95
Luis Villegas <luism.villegasv@gmail.com>
2023-04-07 09:35:10 -0600

    Se agregaron los archivos necesarios y se hizo la primera maquetación HTML

---
+++ game.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame(){
+  // game.fillRect(x,y,w,h);
+  // game.clearRect(x,y,w,h);
+  // game.font = '25px Verdana';
+  // game.fillStyle = 'purple';
+  // game.textAlign = 'center';
+  // game.fillText('Texto', x,y);
+}
